2e841122e2e38134c4fdae39aee4fe42294874c0
young-geng <young.gengxy@gmail.com>
2016-04-23 02:53:56 -0700

    Added Colley's method.

--- rating_utils.py
+++ rating_utils.py
@@ -263,3 +263,101 @@ class MarkovScoreMethod(RatingAlgorithm):
         t2 = self._team_idx[team2]
 
         return self._ratings[t1] / (self._ratings[t1] + self._ratings[t2])
+
+
+class ColleyMethod(RatingAlgorithm):
+
+    def __init__(self):
+        pass
+
+    def _count_match(self):
+        self._match_result_count = np.zeros((self._n_teams, self._n_teams))
+
+        for i in xrange(len(self._data)):
+
+            if self._data['HomePTS'][i] >= self._data['VisitorPTS'][i]:
+                self._match_result_count[
+                    self._team_idx[self._data['Home'][i]],
+                    self._team_idx[self._data['Visitor'][i]]
+                ] += 1
+
+            if self._data['HomePTS'][i] <= self._data['VisitorPTS'][i]:
+                self._match_result_count[
+                    self._team_idx[self._data['Visitor'][i]],
+                    self._team_idx[self._data['Home'][i]]
+                ] += 1
+
+        return self._match_result_count
+
+    def fit(self, data, teams=None):
+
+        if teams is None:
+            teams = get_teams(data)
+
+        self._team_names = teams
+        self._data = data
+        self._n_teams = len(teams)
+
+        self._team_idx = {}
+        for i in xrange(len(teams)):
+            self._team_idx[teams[i]] = i
+
+        match_matrix = self._count_match()
+
+        C = match_matrix + match_matrix.T + 2
+
+        C = C - np.diag(np.sum(match_matrix, axis=0) + np.sum(match_matrix, axis=1) + 2)
+
+        b = 1 + (np.sum(match_matrix, axis=1) - np.sum(match_matrix, axis=0)) / 2
+
+        self._ratings = np.linalg.lstsq(C, b)[0]
+
+        self._optimize_logistic_parameter()
+
+    @property
+    def ratings(self):
+        return self._ratings
+
+    def _correct_probability(self, p):
+        epsilon = 0.01
+        if p == 0:
+            return p + epsilon
+        elif p == 1:
+            return p - epsilon
+        return p
+
+    def _optimize_logistic_parameter(self):
+        x = []
+        p = []
+
+        matches = self._count_match()
+        for i in xrange(self._n_teams):
+            for j in xrange(i):
+                w1 = matches[i, j]
+                w2 = matches[j, i]
+                if w1 + w2 == 0:
+                    # Haven't had a match in the training set
+                    continue
+                x.append(self._ratings[i] - self._ratings[j])
+                p.append(self._correct_probability(w1 / (w1 + w2)))
+
+        x = np.array(x)
+        p = np.array(p)
+
+        y = -np.log(1 / p - 1)
+
+
+        self._logistic_scale = np.sum(x * y) / np.sum(x * x)
+
+    @property
+    def logistic_scale(self):
+        return self._logistic_scale
+
+    def _logistic_probability(self, x):
+        return 1 / (1 + np.exp(-x * self._logistic_scale))
+
+    def predict(self, team1, team2):
+        return self._logistic_probability(
+            self._ratings[self._team_idx[team1]]
+            - self._ratings[self._team_idx[team2]]
+        )
